ecdde764be4f7c4592f08faa2d2fa3e74c64b0a8
Muhammad Fidaul Haq <vivalafida123@gmail.com>
2024-11-03 19:33:35 +0700

    Updateheee

---
+++ Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("Play 1");
+    }
+
+}
